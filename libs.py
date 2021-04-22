9686f36afa17e4df0bc44135e31f3209644df159
colinxu2020 <XuJiaShu2020@outlook.com>
2021-04-22 18:25:33 +0800

    code format and add docstrings

--- libs.py
+++ libs.py
@@ -1,20 +1,28 @@
+"""
+The library of pywallpaper.
+
+Author: colinxu2020
+
+Functions:
+    read_config: read config file
+    get_source: get default source
+    get_wallpaper: get wallpaper
+    write_file_like_object_text_to_file: write file like object's text to file
+    set_wallpaper: set wallpaper
+"""
+
 import urllib.request as req
 from os.path import abspath
 import typing
 import toml
 
 
-__author__ = 'colinxu2020'
-
-
 def read_config(Key: str) -> typing.Any:
     """
-    Argments:
-        Key: 接受字符串,要读取的配置信息
-    Return:
-        不固定类型,为实际配置信息
+    Read config file.
 
-    从config.toml读取指定的配置信息
+    Arguments:
+        Key: key name of the configuration information to be read
     """
     with open('config.toml') as f:
         config = toml.load(f)[Key]
@@ -22,27 +30,18 @@ def read_config(Key: str) -> typing.Any:
 
 
 def get_source() -> dict[str, str]:
-    """
-    Argment:
-        None
-    Return:
-        None
-
-    获取默认源
-    """
+    """Get default source."""
     AutoSource = read_config('autoSource')
     return read_config('source')[AutoSource]
 
 
 def get_wallpaper(day: int = 0) -> tuple:
     """
-    Argments:
-        day: 接受Int类型的数据,默认为0,0代表今天,1代表明天,-1代表昨天,以此类推
-    Return:
-        返回一个元组,包含图片名和链接
+    Get wallpaper from source in config(include image name and image url).
 
-    获取壁纸
-    """
+    Arguments:
+        day： 0 for today, 1 for tomorrow, -1 for yesterday, and so on (default: 0)
+    """  # noqa: E501
     source = get_source()
     day = -day
     url = source['url'].format(day=day)
@@ -57,30 +56,31 @@ def get_wallpaper(day: int = 0) -> tuple:
 
 
 def write_file_like_object_text_to_file(
-        file_like_object, filepath: str, mode: str = 'w') -> None:
+        file_like_object, filepath: str,
+        mode: typing.Optional[str] = None) -> None:
     """
-    Argment:
-        Object:FileLikeObject
-        filepath:要写入到的文件路径
-        byte:bool
-    Return:
-        None
+    Write text of object has read attribute to file.
 
-    将一个FileLikeObject写入到文件
+    Argument:
+        file_like_object: file link object
+        filepath: path of the file will write to.
+        mode: the mode of open file will write to.
     """
+    if mode is None:
+        mode = 'w' + file_like_object.mode[1:]
     with open(filepath, mode) as fp:
+        content = file_like_object.read()
         assert fp.writable(), 'file object most be writable'
-        fp.write(file_like_object.read())
+        assert content, 'text in file_link_objct most not None'
+        fp.write(content)
 
 
 def set_wallpaper(index: int = 0) -> None:
     """
-    Argments:
-        index: 接受整形,图片日期,以当日为0
-    Return:
-        None
+    Set wallpaper from source in config.
 
-    设置壁纸
+    Arguments:
+        index: 接受整形,图片日期,以当日为0
     """
     import ctypes
 
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+"""
+PyWallpaper main entry point.
+
+Author: colinxu2020
+"""
+
 from libs import set_wallpaper
 
 
-__author__='colinxu2020'
-
-if __name__=='__main__':
-    day=input('请输入日期(0代表今天,1代表明天,-1代表昨天,以此类推)')
+if __name__ == '__main__':
+    day = input('请输入日期(0代表今天,1代表明天,-1代表昨天,以此类推)')
     set_wallpaper(int(day))
